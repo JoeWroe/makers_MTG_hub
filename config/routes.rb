d4b48bad45149ede50c7277f34c1ba4857601984
Jamie Brown <jamiebrown201@hotmail.com>
2016-10-15 17:38:44 +0100

    Devise redo (#3)
    Add devise gem, along with a user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  root to: "home#index"
+
   get 'events/index'
 
   get 'events/show'
@@ -8,6 +12,4 @@ Rails.application.routes.draw do
   get 'events/update'
 
   get 'events/destroy'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
